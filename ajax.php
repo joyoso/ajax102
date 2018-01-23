1ab436756d3c1715ed1355e6e6f709467e5bebfc
joyoso <joyfrench99@gmail.com>
2018-01-23 10:23:16 +0100

    ajaxx

---
+++ ajax.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<script text="text/javascript" src="ajax.js"></script>
+	</head>
+	<style>
+		table,th,td {
+		  border : 1px solid black;
+		  border-collapse: collapse;
+		}
+		th,td {
+		  padding: 5px;
+		}
+	</style>
+	<body>
+		<button type="button" onclick="loadDoc()">cibo</button>
+		<br><br>
+		<table id="demo"></table>
+	</body>
+</html>
